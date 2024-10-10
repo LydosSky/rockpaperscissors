d3adfab6906d4d233435a8f3ce4c1cc0217c7a38
Lydos <lydossky@gmail.com>
2024-10-10 14:29:36 +0300

    human choice logic

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-/*
+/**
  * Produces random number interval [0,(num-1)]
  * */
 const randomNumberGenerator = (num) => Math.floor(Math.random() * num);
 
-/*
+/**
  * Given num which is [0,2]
  * returns
  *   "rock"     for 0
@@ -12,3 +12,16 @@ const randomNumberGenerator = (num) => Math.floor(Math.random() * num);
  * */
 const getComputerChoice = (num) =>
   num === 0 ? "rock" : num === 1 ? "paper" : "scissors";
+
+/**
+ * Gets human choice for rock, paper, scissors.
+ * Removes whitespace and lowercase given choice.
+ * */
+const getHumanChoice = () =>
+  prompt("Enter rock, paper, or scissors").trim().toLowerCase();
+
+/**
+ * Check for valid human choice
+ * */
+const validateHumanChoice = (choice) =>
+  choice === "rock" || choice === "paper" || choice === "scissors";
